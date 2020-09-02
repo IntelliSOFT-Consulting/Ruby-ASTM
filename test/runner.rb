7b58a39359451a07f618d8103fed65677734c119
Bhargav Raut <bhargav.r.raut@gmail.com>
2020-09-02 06:34:53 +0530

    redis server not required

--- test/runner.rb
+++ test/runner.rb
@@ -1,5 +1,4 @@
-require '/home/bhargav/Github/Ruby-ASTM/lib/ruby_astm.rb'
-#require 'ruby_astm.rb'
+require 'ruby_astm.rb'
 server = AstmServer.new([{:server_ip => "127.0.0.1", :server_port => 3000}],[],nil,nil,{:use_mappings => false, :log => true, :log_output_directory => "/home/bhargav/Desktop", :output_options => {"format" => "csv", "records_per_file" => "single", "output_directory" => "/home/bhargav/Desktop"}})
 
 server.start_server
