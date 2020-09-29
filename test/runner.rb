c95bac47e9b67863dca3f4c122380109cf91f7cf
moshonk <omondi.ken@gmail.com>
2020-09-29 18:06:20 +0300

    Changed output locations

--- test/runner.rb
+++ test/runner.rb
@@ -3,7 +3,7 @@
 # bundle exec ruby runner.rb
 require '../lib/ruby_astm.rb'
 
-server = AstmServer.new([{:server_ip => "127.0.0.1", :server_port => 3000}],[],nil,nil,{:use_mappings => false, :log => true, :log_output_directory => "/home/bhargav/Github/Ruby-ASTM/output_files", :output_options => {"format" => "csv", "records_per_file" => "single", "output_directory" => "/home/bhargav/Github/Ruby-ASTM/output_files"}})
+server = AstmServer.new([{:server_ip => "127.0.0.1", :server_port => 3000}],[],nil,nil,{:use_mappings => false, :log => true, :log_output_directory => "/opt/Ruby-ASTM/output_files", :output_options => {"format" => "csv", "records_per_file" => "single", "output_directory" => "/opt/Ruby-ASTM/output_files"}})
 
 server.start_server
 
